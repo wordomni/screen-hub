da929d2b4f082757ee533cc54d99a98413e40e00
wordomni <voltusa.project@gmail.com>
2026-05-03 14:37:05 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ function setColor(color) {
   document.getElementById("screen").style.background = color;
 }
 
+/* Fullscreen toggle */
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
   } else {
     document.exitFullscreen();
   }
+}
+
+/* Flicker mode (for pixel fixing) */
+let flickerInterval;
+
+function startFlicker() {
+  const colors = ["red", "green", "blue", "white", "black"];
+  let i = 0;
+
+  flickerInterval = setInterval(() => {
+    document.getElementById("screen").style.background = colors[i % colors.length];
+    i++;
+  }, 100);
+}
+
+function stopFlicker() {
+  clearInterval(flickerInterval);
+}
+
+/* Gradient mode */
+function setGradient() {
+  document.getElementById("screen").style.background =
+    "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)";
 }
